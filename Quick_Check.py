b3f281de49e4ef3df0d91fa2f651155efd9b7700
Robert Piazza <piazza_robert@bah.com>
2019-06-17 16:23:42 -0700

    updates with path

--- Quick_Check.py
+++ Quick_Check.py
@@ -8,8 +8,9 @@ Created on Wed May 29 18:27:37 2019
 #Needed libraries
 #Standard data manipulation and display libraries
 import numpy as np #Workhorse of the data manipulations
-import pandas as pd #Will be needed for some 
+import pandas as pd #Will be needed for some
 from matplotlib import pyplot as plt
+import os
 
 #Various utilities
 import math #calcuting distances between points
@@ -18,10 +19,11 @@ import re # for renaming the solution images so we can retain original name and 
 
 import cv2 as cv #This needs to be 3.4.1!! Based on stackoverflow, I used "conda install -c menpo opencv" to install 3.4.1
 
-from sklearn.cluster import KMeans 
+from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from sklearn.cluster import AffinityPropagation
 
+os.chdir('C:\\Users\\593787\\Documents\\GitHub\\Puzzles\\Puzzles')
 
 #%% Best Image match:
 
@@ -33,7 +35,7 @@ def best_image(canvas, piece, Hessian_distance = 400, threshold_distance = 50, n
     piece_color = cv.imread(piece)
     horizantal_pieces = 27 #number of actual puzzle pieces across- found by completing the edges of the puzzle
     vertical_pieces = 38  #same as above but for height, I believe these are fairly standard for this size puzzle
-    
+
     h_width = scene_color.shape[1]/horizantal_pieces
     v_width = scene_color.shape[0]/vertical_pieces
 
@@ -49,7 +51,7 @@ def best_image(canvas, piece, Hessian_distance = 400, threshold_distance = 50, n
             (int((h-.9)*h_width), int((v-.1)*v_width)), cv.FONT_HERSHEY_PLAIN, .75, (255,255,255))
     #scene_color will be re-used when drawing the matches but is ignored for actual matching
     #For matching, we'll use grayscale
-            
+
     img_object = cv.imread(piece, cv.IMREAD_GRAYSCALE)
     img_scene = cv.imread(canvas, cv.IMREAD_GRAYSCALE)
     if img_object is None or img_scene is None:
@@ -71,7 +73,7 @@ def best_image(canvas, piece, Hessian_distance = 400, threshold_distance = 50, n
     search_params = dict(checks = 50)
 
     flann = cv.FlannBasedMatcher(index_params, search_params)
-    
+
     #SURF uses k-nearest-neighbors for matching the descriptors
     knn_matches = flann.knnMatch(descriptors_obj,descriptors_scene,k=2)
 
@@ -94,7 +96,7 @@ def best_image(canvas, piece, Hessian_distance = 400, threshold_distance = 50, n
     named_pts = pd.DataFrame()
     for index, pt in enumerate(xy_points):
         named_pts['i'+str(index)] = pd.DataFrame(xy_points, columns = ['x', 'y']).apply(lambda row: math.hypot(row.x-pt[0], row.y-pt[1]), axis = 1)
-    
+
     #We have a grid of distances, apply a filter of points who meet the criteria
     #apply the filters of threshold_distance and number of points
     good_bool = list((named_pts < threshold_distance).sum()>num_points)
@@ -104,14 +106,14 @@ def best_image(canvas, piece, Hessian_distance = 400, threshold_distance = 50, n
     if ((named_pts < threshold_distance).sum()>num_points).sum() < 2:
         return
     final_keypoints = (pd.DataFrame([keypoints_scene[mat.trainIdx].pt for mat in good_matches]).drop_duplicates()).values.tolist()
-    
+
     #we don't know how many clusters we're going to use so I'm using the silhouette score
-    #to choose the number of clusters with the highest score. 
-    
-    #Probably could have used affinity clustering on this one as well. 
+    #to choose the number of clusters with the highest score.
+
+    #Probably could have used affinity clustering on this one as well.
     silhouette = 0 #initial score to beat
     n_clusters = 2 #minimum number of clusters
-    
+
     #max number of clusters is the number of keypoints minus 2
     for clusters in range(2,len(final_keypoints)-2):
         km = KMeans(n_clusters=clusters)
@@ -122,15 +124,15 @@ def best_image(canvas, piece, Hessian_distance = 400, threshold_distance = 50, n
     km = KMeans(n_clusters=n_clusters)
     alldistances = km.fit_transform(final_keypoints)
     (pd.DataFrame(final_keypoints).drop_duplicates()).values.tolist()
-    
+
     #-- Draw matches
     img_matches = np.empty((max(piece_color.shape[0], scene_color.shape[0]), piece_color.shape[1]+scene_color.shape[1], 3), dtype=np.uint8)
     cv.drawMatches(piece_color, keypoints_obj, scene_color, keypoints_scene, good_matches, img_matches, matchColor = (128, 0, 0), flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-    
+
     #draw rectangles of the desired threshold distance around each cluster center
     for center in km.cluster_centers_:
         cv.rectangle(img_matches, (int(center[0])-threshold_distance+img_object.shape[1], int(center[1])-threshold_distance), (int(center[0])+threshold_distance+img_object.shape[1], int(center[1])+threshold_distance), (0, 0,255), 5)
-    
+
     #-- Localize the object
     obj = np.empty((len(good_matches),2), dtype=np.float32)
     scene = np.empty((len(good_matches),2), dtype=np.float32)
@@ -162,7 +164,7 @@ def best_image(canvas, piece, Hessian_distance = 400, threshold_distance = 50, n
         obj_corners[3,0,0] = 0
         obj_corners[3,0,1] = img_object.shape[0]
         scene_corners = cv.perspectiveTransform(obj_corners, H)
-    
+
     #Old code for drawing a green rectangle around the most probable location
     #-- Draw lines between the corners (the mapped object in the scene - image_2 )
     #cv.line(img_matches, (int(scene_corners[0,0,0] + img_object.shape[1]), int(scene_corners[0,0,1])),\
@@ -173,7 +175,7 @@ def best_image(canvas, piece, Hessian_distance = 400, threshold_distance = 50, n
     #    (int(scene_corners[3,0,0] + img_object.shape[1]), int(scene_corners[3,0,1])), (0,255,0), 4)
     #cv.line(img_matches, (int(scene_corners[3,0,0] + img_object.shape[1]), int(scene_corners[3,0,1])),\
     #    (int(scene_corners[0,0,0] + img_object.shape[1]), int(scene_corners[0,0,1])), (0,255,0), 4)
-    
+
     #Use that scene corners instead for listing the centroid of the translated points and put the grid location on the left side
         solution_center_x = int((scene_corners[:,0,0].mean()/img_scene.shape[1])*horizantal_pieces*h_width + h_width/2)
         cols_right = int((scene_corners[:,0,0].mean()/img_scene.shape[1])*horizantal_pieces)+1
@@ -185,7 +187,7 @@ def best_image(canvas, piece, Hessian_distance = 400, threshold_distance = 50, n
         cv.putText(img_matches, \
         f'R:{rows_down}', \
         (20, img_object.shape[0]+200), cv.FONT_HERSHEY_COMPLEX, 4, (255,255,255))
-    
+
     #-- Show detected matches
     cv.imwrite(re.sub(r'.(jpg|png)', '_solution.jpg', piece), img_matches)
     print(f"Saved {re.sub(r'.(jpg|png)', '_solution.jpg', piece)}")
@@ -197,7 +199,7 @@ def best_image(canvas, piece, Hessian_distance = 400, threshold_distance = 50, n
 
 #%% Camera Capture
 
-cam = cv.VideoCapture(0)
+cam = cv.VideoCapture(1)
 
 cv.namedWindow("Preview")
 
@@ -221,13 +223,13 @@ while True:
         break
     elif k%256 == 32:
         # SPACE pressed
-        img_name = "opencv_frame.png"#.format(img_counter)
+        img_name = "opencv_frame2.png"#.format(img_counter)
         crop_img = frame[y:y+h, x:x+w]
-        cv.imwrite(img_name, frame)        
-        print("{} written!".format(img_name))   
+        cv.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
         img_counter += 1
         try:
-            best_image('gap.jpg', 'opencv_frame.png')
+            best_image('gap.jpg', 'opencv_frame2.png')
         except ValueError:
             "Value Error"
 cam.release()
